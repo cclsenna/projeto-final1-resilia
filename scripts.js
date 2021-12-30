8b9a8ae3a191f2896b6949e0a153bd2dbbeb411a
Caio Senna <caiosenna93@gmail.com>
2021-12-29 22:33:06 -0300

    adicionando novos arquivos e atualicoes no layout

---
+++ scripts.js
@@ -0,0 +1,13 @@
+
+
+let botaoInicio= ()=>{
+
+    document.getElementById('container-intro').style.display='none';
+    document.getElementById('container-choice').style.display='block';
+
+
+
+    
+
+
+}
