7042a87d0e64e0d7d788e6c66fa2db85b10de553
Caio Senna <caiosenna93@gmail.com>
2022-01-06 23:02:20 -0300

    ajustes tela inicial

--- scripts.js
+++ scripts.js
@@ -4,10 +4,14 @@ let botaoInicio= ()=>{
 
     document.getElementById('container-intro').style.display='none';
     document.getElementById('container-choice').style.display='block';
-
-
-
     
+
+}
+
+let botaoVolta=()=>{
+    document.getElementById('container-intro').style.display='block';
+    document.getElementById('container-choice').style.display='none';
+
 
 
 }
