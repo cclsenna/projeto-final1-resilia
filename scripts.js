6246b23b5ce0c798227aac78c446cdecd62d4476
Caio Senna <caiosenna93@gmail.com>
2022-01-08 18:40:37 -0300

    pagina gameover e estrutura de escolha

--- scripts.js
+++ scripts.js
@@ -12,6 +12,19 @@ let botaoVolta=()=>{
     document.getElementById('container-intro').style.display='block';
     document.getElementById('container-choice').style.display='none';
 
+}
 
+let respostaFase=(fase)=>{
+
+    let resposta=prompt("Para onde deseja seguir?");
+
+    switch(fase){
+        case 'fase1':
+            console.log('entrou no case1');
+    }
+
+   
+  
 
 }
+
